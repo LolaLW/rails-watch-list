282d6bc40e2f19a6d444cb7a4b3961f3c62ab1b2
Lolita Lugand <lyanlola77@gmail.com>
2022-08-18 12:09:13 +0200

    Prepare rails app with external specs

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,5 @@
 class Movie < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
-  has_many :lists, through: :bookmarks
 
   validates :title, presence: true, uniqueness: true
   # validates :photo, presence: true
